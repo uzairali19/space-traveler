5c7b1c805ee5cf98aa068c5547a322a9ba166960
brytebee <brytebee@gmail.com>
2021-12-20 13:52:36 +0100

    :white_check_mark: Update routes
    Add my profile route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/Rockets" element={<Rockets />} />
         <Route path="/Missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
---
+++ src/components/Profile.js
@@ -0,0 +1,8 @@
+const Profile = () => (
+  <div className="container">
+    <h1>Profile</h1>
+    <p>This is demo profile</p>
+  </div>
+);
+
+export default Profile;
