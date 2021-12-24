9082300d3a16d19aca09bd6d2e5bedd312574bc3
Uzair Ali <uzairali19b@gmail.com>
2021-12-24 21:12:46 +0500

    update project dependencies

--- src/components/mission/MissionsProfile.js
+++ src/components/mission/MissionsProfile.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const MissionsProfile = () => {
-  const allMissions = useSelector((state) => state.addRemover);
+  const allMissions = useSelector(
+    (state) => state.addRemover,
+  );
 
   return (
     <div className="container">
       <h3>My Missions</h3>
       {allMissions.map((missions) => (
         <div key={missions.mission_id}>
-          <p key={missions.mission_id}> {missions.mission_name} </p>
+          <p key={missions.mission_id}>
+            {missions.mission_name}
+          </p>
         </div>
       ))}
     </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
