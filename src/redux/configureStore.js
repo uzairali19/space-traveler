5e175c09d1f3555dc987f68660d9038bcd404218
brytebee <brytebee@gmail.com>
2021-12-24 07:44:58 +0100

    no changes

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import missionReducer from './missions/missions';
+import add_remove_reducer from './missions/storeMissions';
 
 const reducer = combineReducers({
   missionReducer,
+  add_remove_reducer,
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
